43cd974e053efa4d72f4ba4722707c0054e87c9e
Patrick Mast <patrick@winfakt.com>
2023-06-25 09:30:57 +0200

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -1,19 +1,31 @@
 import express from 'express';
 import cors from 'cors';
-import shortid from 'shortid';
 
 export const app = express();
 
 app.use(cors({ origin: true }));
-
 app.use(express.json());
 
 const urlDatabase = {};
+let idCounter = 1;
+
+// Custom function to generate short IDs
+function generateShortId() {
+  const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let shortId = '';
+
+  for (let i = 0; i < 6; i++) {
+    const randomIndex = Math.floor(Math.random() * chars.length);
+    shortId += chars.charAt(randomIndex);
+  }
+
+  return shortId;
+}
 
 // Endpoint to shorten a URL
 app.post('/shorten', (req, res) => {
   const longUrl = req.body.url;
-  const shortUrl = shortid.generate();
+  const shortUrl = generateShortId();
 
   urlDatabase[shortUrl] = longUrl;
 
@@ -29,7 +41,7 @@ app.get('/:shortUrl', (req, res) => {
     return res.status(404).send({ error: 'Short URL not found' });
   }
 
-  res.redirect(301, longUrl);
+  return res.redirect(301, longUrl);
 });
 
 // Healthcheck endpoint
